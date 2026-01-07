f715017e9f973018c9a9b25ea805788343c1b50d
Deepak Valechha <d_val26@pm.me>
2026-01-07 09:19:35 +0530

    Test Fixes

--- test/views/login_screen_test.dart
+++ test/views/login_screen_test.dart
@@ -5,12 +5,25 @@ import 'package:provider/provider.dart';
 import 'package:my_reminder/providers/auth_provider.dart';
 import 'package:my_reminder/providers/navigation_model.dart';
 import 'package:my_reminder/providers/user_profile_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../helpers/test_setup.dart';
 
 // Mock Provider if needed, but for simple widget finding we might not need full mocks if we wrap in Provider
 class MockAuthProvider extends ChangeNotifier implements AuthProvider {
   @override
   bool isLoading = false;
+
+  @override
+  bool get isPasswordResetFlow => false;
+
+  @override
+  User? get user => null;
+
+  @override
+  bool get isAuthenticated => false;
+
+  @override
+  bool get isEmailVerified => false;
 
   @override
   dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
@@ -46,8 +59,8 @@ void main() {
     // Verify password field exists
     expect(find.widgetWithText(TextFormField, 'Password'), findsOneWidget);
 
-    // Verify login button exists
-    expect(find.widgetWithText(ElevatedButton, 'Login'), findsOneWidget);
+    // Verify login button exists (button text is "Log in" not "Login")
+    expect(find.widgetWithText(ElevatedButton, 'Log in'), findsOneWidget);
 
     // Verify forgot password button exists
     expect(find.text('Forgot Password?'), findsOneWidget);
